eb79cca1d1dcc09761b95f2ac8476ef7100088ab
Jan Wiśniak <10376862u@qed.local>
2025-02-06 15:11:17 +0100

    pierwszy commit

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.Notification;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.notification.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "my_channel_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v->sendNotification());
+
+        Button buttonLong = findViewById(R.id.buttonLong);
+        buttonLong.setOnClickListener(v->sendNotificationLong());
+
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Kanal Powiadomien";
+            String description = "Opis Kanalu Powiadomien";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+
+    private void sendNotification(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Nowe powiadomienie 3TP")
+                    .setContentText("Tresc powiadomienia dla klasy 3TP")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(1,builder.build());
+        }
+
+    }
+    private void sendNotificationLong(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Nowe powiadomienie 3TP")
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Tresc powiadomienia dla klasy 3TPTresc powiadomienia dla klasy 3TPTresc powiadomienia dla klasy 3TPTresc powiadomienia dla klasy 3TPTresc powiadomienia dla klasy 3TPTresc powiadomienia dla klasy 3TP"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(1,builder.build());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notification/NotificationHelper.java
@@ -0,0 +1,77 @@
+package com.example.notification;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.CaseMap;
+import android.os.Build;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+public class NotificationHelper {
+    private  static final String CHANNEL_ID="default_channel";
+    private static final String CHANNEL_NAME = "Kanał powiadomień";
+    
+    public static void sendNotification(int Notification_ID, AppCompatActivity activity,
+                                        String title, String message, int styleType,
+                                        int largeIconResID){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.POST_NOTIFICATIONS)
+                    !=PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 100);
+                return;
+            }
+        };
+        NotificationManager notificationManager = (NotificationManager)
+                activity.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder =
+                new NotificationCompat.Builder(activity, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+        {
+            
+        }
+        if (largeIconResID != null) {
+            Bitmap largeIcon = BitmapFactory.decodeResource(activity.getResources(), largeIconResID);
+            Notification.Builder builder;
+            builder.setLargeIcon(largeIcon);
+        }
+        switch(styleType){
+            case 1:
+                Notification.Builder builder;
+                builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
+                break;
+            case 2:
+                if (largeIconResID !=0){
+                    Bitmap largeIcon = BitmapFactory.decodeResource(activity.getResources(), largeIconResID);
+                    builder.setLargeIcon(largeIcon);
+                }
+                break;
+            case 3:
+                NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+                inboxStyle.addLine(message);
+                inboxStyle.addLine("Dodatkowa linia tekstu");
+                builder.setStyle(inboxStyle);
+                break;
+        }
+        
+        
+    }
+}
